28013f989fb73de50f672852b351ebb3580c1570
Obrin <jordan@iflix.com>
2016-03-25 00:07:43 +0800

    set transition when clicking navigation

--- src/javascripts/navigation.js
+++ src/javascripts/navigation.js
@@ -31,3 +31,13 @@ $(window).scroll(function addActiveClassNavigation() {
     }
   })
 });
+
+
+// Page scrolling feature
+$('a.page-scroll').bind('click', function(event) {
+  var link = $(this);
+  $('html, body').stop().animate({
+    scrollTop: $(link.attr('href')).offset().top - 70
+  }, 500);
+  event.preventDefault();
+});
